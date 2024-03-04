9cb4658a96ec469598f7a43cc5f5002993fbc323
ystk3411 <yoshi.hama0701@gmail.comgit push -u origin main>
2024-03-04 18:06:20 +0900

    指摘点を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   private
 
   def current_cart
-    current_cart = Cart.find_by(id: session[:cart_id]) || Cart.create
+    current_cart = Cart.find_or_create_by(id: session[:cart_id])
     session[:cart_id] ||= current_cart.id
     current_cart
   end
