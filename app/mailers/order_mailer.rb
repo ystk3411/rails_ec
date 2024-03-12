a09d57367e40127829c6e2f42f5578787670b7bf
ystk3411 <yoshi.hama0701@gmail.comgit push -u origin main>
2024-03-12 22:32:36 +0900

    ヘルパーを追加

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,9 +1,11 @@
 class OrderMailer < ApplicationMailer
   default from: 'kobazon@example.com'
+  helper CartsHelper
 
   def order_email(email,item)
     @order = email
     @items = item
+    @price_total = number_to_currency(cart_price_total,locale:'jp')
     mail(to: @order.email, subject: 'ご購入ありがとうございます')
   end
 end
