ee0946c3e0eb89013e566f1c7089e8b93a4004c4
ystk3411 <yoshi.hama0701@gmail.comgit push -u origin main>
2024-03-26 23:16:33 +0900

    使用済みのプロモーションコードを使用不可に修正

--- app/controllers/promo_codes_controller.rb
+++ app/controllers/promo_codes_controller.rb
@@ -4,8 +4,10 @@ class PromoCodesController < ApplicationController
   def discount
     code = PromoCode.find_by(code: params[:promo_code][:code])
 
-    if code
+    if code && code.status == true
       session[:register_code] = params[:promo_code][:code]
+      code.status = false
+      code.save
     else
       flash[:danger] = '入力されたコードは無効です'
     end
